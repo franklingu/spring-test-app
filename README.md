12b48d59f98df434158c72112e726356b15c05f8
franklingu <franklingujunchao@gmail.com>
2018-02-14 18:42:20 +0800

    Add initial setup

---
+++ README.md
@@ -0,0 +1,6 @@
+Spring Boot Application Demo
+=================================================
+
+This repo demos basic use of Spring Boot + Spring Data and Tests. You can use it for a simple starter project or reference for project setup.
+
+
---
+++ src/test/java/hello/MainControllerTest.java
@@ -0,0 +1,45 @@
+package hello;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MainControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testAddNewUserOK() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/demo/add?name=test&email=test")
+                .accept(MediaType.ALL))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Saved")));
+    }
+
+    @Test
+    public void testAddNewUserMissingParamter() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/demo/add?name=test")
+                .accept(MediaType.ALL))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void testGetAllUsersOK() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/demo/all")
+                .accept(MediaType.ALL))
+                .andExpect(status().isOk());
+    }
+}
